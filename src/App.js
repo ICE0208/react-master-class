0a135a0abb621446cb7f9c81c6a427af315881d4
ICE0208 <doky3307@naver.com>
2023-09-05 15:57:51 +0900

    (#2.1) Styled Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
 function App() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Father style={{ display: "flex" }}>
+      <BoxOne>
+        <Text>Hello</Text>
+      </BoxOne>
+
+      <BoxTwo />
+    </Father>
   );
 }
 
