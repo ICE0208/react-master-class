2600e94602e6918448b354ed440af218120197ca
ICE0208 <doky3307@naver.com>
2023-09-05 16:40:15 +0900

    (#2.3) As & Attrs

--- src/App.js
+++ src/App.js
@@ -3,22 +3,18 @@ import styled from "styled-components";
 const Father = styled.div`
   display: flex;
 `;
-const Box = styled.div`
-  background-color: ${(props) => props.bg};
-  width: 100px;
-  height: 100px;
-`;
 
-const Circle = styled(Box)`
-  border-radius: 50%;
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bg="teal" />
-      <Box bg="tomato" />
-      <Circle bg="#b8b8b8" />
+    <Father as="header">
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
