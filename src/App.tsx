de1fec6720ac2a22706784dd6d4fd4f317d4f432
ICE0208 <doky3307@naver.com>
2023-09-09 00:03:59 +0900

    (#3.5) Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import React from "react";
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  // event type 예시
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // target대신 currentTarget
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
   return (
     <div>
-      <Circle
-        bgColor="teal"
-        borderColor="tomato"
-      />
-      <Circle bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
