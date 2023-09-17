3c1c27c3c3511e406ceb53f4227269727a49cf3e
ICE0208 <doky3307@naver.com>
2023-09-17 21:18:12 +0900

    (#6.6) Forms

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,25 +1,56 @@
 import React, { useState } from "react";
 
+import { useForm } from "react-hook-form";
+
+// function ToDoList() {
+//   const [toDo, setTodo] = useState("");
+//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = event;
+//     setTodo(value);
+//   };
+//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     console.log(toDo);
+//   };
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input
+//           type="text"
+//           value={toDo}
+//           onChange={onChange}
+//           placeholder="Write a to do"
+//         />
+//         <button>Add</button>
+//       </form>
+//     </div>
+//   );
+// }
+
 function ToDoList() {
-  const [toDo, setTodo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setTodo(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  const { register, watch } = useForm();
+  console.log(watch());
+
   return (
     <div>
-      <form onSubmit={onSubmit}>
+      <form>
         <input
-          type="text"
-          value={toDo}
-          onChange={onChange}
-          placeholder="Write a to do"
+          {...register("Email")}
+          placeholder="Email"
+        />
+        <input
+          {...register("Username")}
+          placeholder="Username"
+        />
+        <input
+          {...register("Password")}
+          placeholder="Password"
+        />
+        <input
+          {...register("Password Confirm")}
+          placeholder="Password Confirm"
         />
         <button>Add</button>
       </form>
