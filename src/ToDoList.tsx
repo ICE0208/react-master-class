f9437a048b41d4ab1523d265c263f3880d3eb5f9
ICE0208 <doky3307@naver.com>
2023-09-18 21:11:05 +0900

    (#6.11) Add To Do

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 
 import { useForm } from "react-hook-form";
+import { atom, useRecoilState } from "recoil";
+
+interface IToDo {
+  text: string;
+  id: number;
+  category: "TO_DO" | "DOING" | "DONE";
+}
+const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+});
 
 interface IForm {
   toDo: string;
 }
 
 function ToDoList() {
+  // useRecoilValue, useSetRecoilState를 한번에
+  const [toDos, setToDos] = useRecoilState(toDoState);
   const {
     register,
     handleSubmit,
@@ -16,11 +29,16 @@ function ToDoList() {
 
   const handleValid = (data: IForm) => {
     console.log("add to do", data.toDo);
+    setToDos((oldToDos) => [
+      { text: data.toDo, id: Date.now(), category: "TO_DO" },
+      ...oldToDos,
+    ]);
     setValue("toDo", "");
   };
-
   return (
     <div>
+      <h1>To Dos</h1>
+      <hr />
       <form onSubmit={handleSubmit(handleValid)}>
         <input
           {...register("toDo", {
@@ -31,6 +49,11 @@ function ToDoList() {
         <button>Add</button>
         <span>{errors?.toDo?.message?.toString()}</span>
       </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <li key={toDo.id}>{toDo.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
