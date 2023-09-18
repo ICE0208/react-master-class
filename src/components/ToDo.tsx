e9843a2aa716c279b81b686326bf7241ce03a43c
ICE0208 <doky3307@naver.com>
2023-09-19 00:15:46 +0900

    (#6.14) Immutability part One

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,31 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const OnClick = (newCategory: IToDo["category"]) => {
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const oldTodo = oldToDos[targetIndex];
+      const newToDo = { ...oldTodo, category: newCategory };
+      const newToDos = [...oldToDos];
+      newToDos[targetIndex] = newToDo;
+      return newToDos;
+    });
+  };
   return (
     <li>
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== "TO_DO" && (
+        <button onClick={() => OnClick("TO_DO")}>To Do</button>
+      )}
+      {category !== "DOING" && (
+        <button onClick={() => OnClick("DOING")}>DOING</button>
+      )}
+      {category !== "DONE" && (
+        <button onClick={() => OnClick("DONE")}>DONE</button>
+      )}
       <span style={{ marginLeft: "10px" }}>{text}</span>
+      <span style={{ marginLeft: "10px" }}>{category}</span>
     </li>
   );
 }
