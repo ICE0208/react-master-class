0c54f5218bde768b0eb7bbc6419e461bdf8e785b
ICE0208 <doky3307@naver.com>
2023-09-23 00:02:23 +0900

    (#7.3) Drag and Drop part Two

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,38 @@ function App() {
     <DragDropContext onDragEnd={onDragEnd}>
       <div>
         <Droppable droppableId="one">
-          {() => (
-            <ul>
+          {(magic) => (
+            <ul
+              ref={magic.innerRef}
+              {...magic.droppableProps}
+            >
               <Draggable
                 draggableId="first"
                 index={0}
               >
-                {() => <li>One</li>}
+                {(magic) => (
+                  <li
+                    ref={magic.innerRef}
+                    {...magic.draggableProps}
+                  >
+                    <span {...magic.dragHandleProps}>🧊</span>
+                    One
+                  </li>
+                )}
               </Draggable>
               <Draggable
                 draggableId="second"
                 index={1}
               >
-                {() => <li>Two</li>}
+                {(magic) => (
+                  <li
+                    ref={magic.innerRef}
+                    {...magic.draggableProps}
+                  >
+                    <span {...magic.dragHandleProps}>🧊</span>
+                    Two
+                  </li>
+                )}
               </Draggable>
             </ul>
           )}
