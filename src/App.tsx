b6aed3bd20da8d97d36a7a0b6fd4d0cd1f594394
ICE0208 <doky3307@naver.com>
2023-09-28 01:11:45 +0900

    (#8.9) MotionValues part Three

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
 import styled from "styled-components";
-import {
-  Variants,
-  motion,
-  useMotionValue,
-  useMotionValueEvent,
-  useTransform,
-} from "framer-motion";
-import { useEffect, useRef } from "react";
+import { motion, useMotionValue, useTransform, useScroll } from "framer-motion";
 
-const Wrapper = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.3);
-  border-radius: 40px;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
+  /* width: 100vw; */
   display: flex;
   justify-content: center;
   align-items: center;
   overflow: hidden;
+  background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 
 // motion div에 styled 적용
@@ -36,22 +20,23 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants: Variants = {
-  click: { scale: 1, borderRadius: "100px" },
-  drag: { backgroundColor: "rgb(46, 204, 113)" },
-};
-
 function App() {
   const x = useMotionValue(0);
-  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0]);
-  useMotionValueEvent(x, "change", (l) => {
-    console.log(scale.get());
-  });
-
+  const rotateZ = useTransform(x, [-600, 600], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-600, 600],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(233, 227, 36))",
+    ]
+  );
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
   return (
-    <Wrapper>
+    <Wrapper style={{ background: gradient }}>
       <Box
-        style={{ x, scale }}
+        style={{ x, rotateZ, scale }}
         drag="x"
         dragSnapToOrigin
       />
