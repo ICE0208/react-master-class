8cbe5beed4f484eda045d10b66a109f90a40df3c
ICE0208 <doky3307@naver.com>
2023-09-28 00:17:30 +0900

    (#8.5) Gestures part One

--- src/App.tsx
+++ src/App.tsx
@@ -13,67 +13,27 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 30px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const Circle = styled(motion.div)`
   background-color: white;
-  height: 70px;
-  width: 70px;
-  border-radius: 35px;
+  border-radius: 30px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  place-self: center;
 `;
 
 const boxVariants: Variants = {
-  start: {
-    opacity: 0,
-    scale: 0.2,
-  },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: "spring",
-      duration: 1,
-      bounce: 0.1,
-      delayChildren: 0.7,
-      staggerChildren: 0.3,
-    },
-  },
-};
-
-const circleVariants: Variants = {
-  start: {
-    opacity: 0,
-    y: 30,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.5,
-    },
-  },
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "100px" },
+  drag: { backgroundColor: "rgb(46, 204, 113)" },
 };
 
 function App() {
   return (
     <Wrapper>
       <Box
+        drag
         variants={boxVariants}
-        initial="start"
-        animate="end"
-      >
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+        whileHover="hover"
+        whileTap="click"
+        whileDrag="drag"
+      />
     </Wrapper>
   );
 }
