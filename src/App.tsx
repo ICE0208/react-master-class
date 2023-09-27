ae88acb430d7d8c7c1e9048c91181e75cdcf21eb
ICE0208 <doky3307@naver.com>
2023-09-27 23:10:59 +0900

    Animations Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,27 @@
-import { DragDropContext, DropResult } from "react-beautiful-dnd";
-import { useRecoilState } from "recoil";
 import styled from "styled-components";
-import { toDoState } from "./atoms";
-import Board from "./components/Board";
-
-function App() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-  const onDragEnd = (info: DropResult) => {
-    const { destination, source } = info;
-    if (!destination) return;
-    if (destination.droppableId === source.droppableId) {
-      // same board movement.
-      setToDos((oldToDos) => {
-        const boardCopy = [...oldToDos[source.droppableId]];
-        const sourceToDo = boardCopy[source.index];
-        boardCopy.splice(source.index, 1);
-        boardCopy.splice(destination.index, 0, sourceToDo);
-        return {
-          ...oldToDos,
-          [source.droppableId]: boardCopy,
-        };
-      });
-    } else {
-      // cross board movement.
-      setToDos((oldToDos) => {
-        const sourceBoard = [...oldToDos[source.droppableId]];
-        const destinationBoard = [...oldToDos[destination.droppableId]];
-        const sourceToDo = sourceBoard[source.index];
-        sourceBoard.splice(source.index, 1);
-        destinationBoard.splice(destination.index, 0, sourceToDo);
-        return {
-          ...oldToDos,
-          [source.droppableId]: sourceBoard,
-          [destination.droppableId]: destinationBoard,
-        };
-      });
-    }
-  };
-  return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Wrapper>
-        <Boards>
-          {Object.keys(toDos).map((boardId) => (
-            <Board
-              boardId={boardId}
-              key={boardId}
-              toDos={toDos[boardId]}
-            />
-          ))}
-        </Boards>
-      </Wrapper>
-    </DragDropContext>
-  );
-}
-
-const Boards = styled.div`
-  display: grid;
-  width: 100%;
-  gap: 10px;
-  grid-template-columns: repeat(3, 1fr);
-`;
 
 const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
   display: flex;
-  max-width: 1000px;
-  margin: 0 auto;
   justify-content: center;
   align-items: center;
-  height: 100vh;
 `;
+
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <Box />
+    </Wrapper>
+  );
+}
 
 export default App;
