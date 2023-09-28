8f954d1eb7fbc6d7f2325fcfc60b94c21ecadfc9
ICE0208 <doky3307@naver.com>
2023-09-28 20:38:19 +0900

    (#8.14) You Need to Watch This

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import styled from "styled-components";
-import { AnimatePresence, Variants, motion } from "framer-motion";
-import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
   position: relative;
   height: 100vh;
   display: flex;
-  justify-content: center;
-  flex-direction: column;
+  justify-content: space-around;
   align-items: center;
   overflow: hidden;
   background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 200px;
+  width: 300px;
+  height: 300px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   top: 100px;
@@ -23,87 +22,38 @@ const Box = styled(motion.div)`
   justify-content: center;
   align-items: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  position: absolute;
-  top: 50px;
 `;
 
-const box: Variants = {
-  entry: (isBack: number) => ({
-    x: isBack < 0 ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-    zIndex: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    zIndex: 1,
-    transition: {
-      duration: 0.2,
-      type: "spring",
-      bounce: 0.3,
-    },
-  },
-  exit: (isBack: number) => ({
-    x: isBack < 0 ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    zIndex: 0,
-    transition: {
-      duration: 0.2,
-    },
-  }),
-};
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
+  border-radius: 50px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
 function App() {
-  const [[visible, direction], setVisible] = useState([0, 1]);
-  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
 
-  useEffect(() => {
-    setButtonDisabled(true); // 애니메이션이 진행 중일 때 버튼 비활성화
-  }, [visible]);
-
-  const onAnimationComplete = () => {
-    setButtonDisabled(false); // 애니메이션이 끝나면 버튼 활성화
-  };
-
-  const move = (direction: number) => {
-    setVisible((prev) =>
-      prev[0] + direction >= 0 && prev[0] + direction <= 10
-        ? [prev[0] + direction, direction]
-        : prev
-    );
-  };
-
-  console.log(visible, direction);
   return (
-    <Wrapper>
-      <AnimatePresence custom={direction}>
-        <Box
-          custom={direction}
-          variants={box}
-          initial="entry"
-          animate="center"
-          exit="exit"
-          key={visible}
-          onAnimationComplete={onAnimationComplete}
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <button
-        onClick={() => move(1)}
-        disabled={buttonDisabled}
-      >
-        next
-      </button>
-      <button
-        onClick={() => move(-1)}
-        disabled={buttonDisabled}
-      >
-        prev
-      </button>
+    <Wrapper onClick={toggleClicked}>
+      <Box>
+        {clicked && (
+          <Circle
+            layoutId="a"
+            style={{ scale: 1, borderRadius: "50%" }}
+          />
+        )}
+      </Box>
+      <Box>
+        {clicked || (
+          <Circle
+            layoutId="a"
+            style={{ scale: 2, borderRadius: 0 }}
+          />
+        )}
+      </Box>
     </Wrapper>
   );
 }
