05a39ed975c128e55fffbb6048976650fb99cdbb
ICE0208 <doky3307@naver.com>
2023-10-01 00:29:59 +0900

    Init for NOMFLIX CLONE

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,14 @@
-// ! Create a theme
-
 import { DefaultTheme } from "styled-components/dist/types";
 
 export const theme: DefaultTheme = {
-  bgColor: "#3F8CF2",
-  boardColor: "#DADFE9",
-  cardColor: "white",
+  red: "#E51013",
+  black: {
+    veryDark: "#141414",
+    darker: "#181818",
+    lighter: "#2F2F2F",
+  },
+  white: {
+    lighter: "#fff",
+    darker: "#e5e5e5",
+  },
 };
