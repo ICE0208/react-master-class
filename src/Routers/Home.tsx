2063191c89786d03b06b75400ef374e869b3b006
ICE0208 <doky3307@naver.com>
2023-10-02 16:24:23 +0900

    (#9.7) Slider part One

--- src/Routers/Home.tsx
+++ src/Routers/Home.tsx
@@ -1,7 +1,9 @@
+import React, { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { IGetMoviesResult, getMovies } from "../api";
 import styled from "styled-components";
 import { makeImagePath } from "../utils";
+import { AnimatePresence, Variants, motion } from "framer-motion";
 
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>({
@@ -10,8 +12,50 @@ function Home() {
     retry: false,
     refetchOnWindowFocus: false,
   });
+  const [index, setIndex] = useState(0);
+  const increaseIndex = () => setIndex((prev) => prev + 1);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [rowVariants, setRowVariants] = useState<Variants>({
+    hidden: {
+      x: windowWidth,
+    },
+    visible: {
+      x: 0,
+    },
+    exit: {
+      x: -windowWidth,
+    },
+  });
 
-  console.log(data);
+  useEffect(() => {
+    // 윈도우 너비 변경 시 이벤트 핸들러
+    const handleResize = () => {
+      const newWindowWidth = window.innerWidth;
+      setWindowWidth(newWindowWidth);
+
+      // rowVariants 업데이트
+      setRowVariants({
+        hidden: {
+          x: newWindowWidth,
+        },
+        visible: {
+          x: 0,
+        },
+        exit: {
+          x: -newWindowWidth,
+        },
+      });
+    };
+
+    // 윈도우 리사이즈 이벤트 리스너 등록
+    window.addEventListener("resize", handleResize);
+
+    // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <Wrapper>
       {isLoading ? (
@@ -19,11 +63,28 @@ function Home() {
       ) : (
         <>
           <Banner
+            onClick={increaseIndex}
             $bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
           >
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence>
+              <Row
+                key={index}
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+              >
+                {[1, 2, 3, 4, 5, 6].map((i) => (
+                  <Box key={i}>{i}</Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
@@ -33,6 +94,7 @@ function Home() {
 const Wrapper = styled.div`
   background: black;
   height: 200vh;
+  overflow-x: hidden;
 `;
 
 const Loader = styled.div`
@@ -62,6 +124,26 @@ const Title = styled.h2`
 const Overview = styled.p`
   font-size: 28px;
   width: 44%;
+`;
+
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+  color: red;
+  font-size: 64px;
 `;
 
 export default Home;
